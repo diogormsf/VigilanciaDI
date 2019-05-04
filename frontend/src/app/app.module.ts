a89c509c00159301a4d0860154a0da504bd0fb1c
Duarte Silva <fc47826@alunos.fc.ul.pt>
2019-05-04 05:51:56 +0100

    complete header + add side bar (need to finish slide mode)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material';
+import { MatSidenavModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { HomeComponent } from './components/home/home.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/side-bar/side-bar.component.ts
+++ frontend/src/app/components/side-bar/side-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MatSidenav } from '@angular/material';
 
 @Component({
   selector: 'app-side-bar',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
+  private sidenav: MatSidenav;
+
+  menuOpen: any;
+  @Input() menuState: boolean;
+  menuStateAux = true;
+  valueMenu: String;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  checkMenu(){
+    console.log('Sidenav receive check', this.menuState);
+    if(this.menuState == this.menuStateAux){
+      
+      return "opened";
+    }
+    else{
+      this.menuStateAux=this.menuState;
+      return "";
+    }
+  }
+
+  writeValueMenu(){
+    console.log('Sidenav receive', this.menuState);
+  }
+
+ 
+
+
+ 
 }
