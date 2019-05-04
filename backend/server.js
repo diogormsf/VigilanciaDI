100231fe57eb6ccab009fee6e58d2fc04843ba88
TELMO FONTE <telmoalmeidafonte@gmail.com>
2019-05-04 17:09:40 +0100

    added services for Professor & Exame

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
-
-import Professor from './models/Professor';
-import Exame from './models/Exame';
-//import Vigilancia from './models/Vigilancia';
+import Professor from "./models/Professor";
+import Exame from "./models/Exame";
+import Vigilancia from './models/Vigilancia';
 
 const app = express();
 const router = express.Router();
@@ -16,12 +15,95 @@ app.use(bodyParser.json());
 
 //Mongoose connection alternative (localhost)
 //para usar, tem que ter o mongodb compass, criar uma DB local, criar uma DB vigilanciadi
-mongoose.connect('mongodb://localhost:27017/vigilanciadi', { useNewUrlParser: true })
+mongoose.connect("mongodb://localhost:27017/vigilanciadi", {
+  useNewUrlParser: true
+});
 const connection = mongoose.connection;
 
-connection.once('open', () => {
-    console.log('MongoDB database connection established succecssfully!');
-})
+connection.once("open", () => {
+  console.log("MongoDB database connection established succecssfully!");
+});
+
+//------------------- ROUTES -------------------------------
+//------------------- Exame --------------------------------
+//retorna todos os exames
+router.route("/exames").get((req, res) => {
+  Exame.find((err, exames) => {
+    if (err) console.log(err);
+    else res.json(exames);
+  });
+});
+
+//retorn um exame pelo seu ID
+router.route("/exames/:id").get((req, res) => {
+  Exame.findById(req.params.id, (err, exame) => {
+    if (err) console.log(err);
+    else res.json(exame);
+  });
+});
+
+//adiciona um exame
+router.route("/exames/add").post((req, res) => {
+  let exame = new Exame(req.body);
+  exame
+    .save()
+    .then(exame => {
+      res.status(200).json({ exame: "Added successfully" });
+    })
+    .catch(err => {
+      res.status(400).send("Failed to create new record");
+    });
+});
+
+//apaga um exame pelo seu ID
+router.route("/exames/delete/:id").get((req, res) => {
+  Exame.findByIdAndRemove({ _id: req.params.id }, (err, exames) => {
+    if (err) res.json(err);
+    else res.json("Remove successfully");
+  });
+});
+//-----------------------------------------------------------
+//-----------------------------------------------------------
+
+//------------------- Professor -----------------------------
+//retorna todos os professores
+router.route("/professores").get((req, res) => {
+  Professor.find((err, professores) => {
+    if (err) console.log(err);
+    else res.json(professores);
+  });
+});
+
+//retorn um exame pelo seu ID
+router.route("/professores/:id").get((req, res) => {
+  Professor.findById(req.params.id, (err, professor) => {
+    if (err) console.log(err);
+    else res.json(professor);
+  });
+});
+
+//adiciona um exame
+router.route("/professores/add").post((req, res) => {
+  let professor = new Professor(req.body);
+  professor
+    .save()
+    .then(professor => {
+      res.status(200).json({ professor: "Added successfully" });
+    })
+    .catch(err => {
+      res.status(400).send("Failed to create new record");
+    });
+});
+
+//apaga um exame pelo seu ID
+router.route("/professores/delete/:id").get((req, res) => {
+  Professor.findByIdAndRemove({ _id: req.params.id }, (err, professores) => {
+    if (err) res.json(err);
+    else res.json("Remove successfully");
+  });
+});
+//-----------------------------------------------------------
+
 
 /* -------------------------------------------------------------
 //Set up mongoose connection
@@ -31,8 +113,8 @@ mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-------------------------------------------------------------- */ 
+------------------------------------------------------------- */
 
-app.use('/', router);
+app.use("/", router);
 
 app.listen(4000, () => console.log(`Express server running on port 4000`));
