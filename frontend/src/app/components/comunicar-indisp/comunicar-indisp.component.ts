b8fc255e5c8d26d24f1689729c94cdf2e2ca4149
Diogo Fernandes <fc47809@alunos.fc.ul.pt>
2019-05-05 02:11:15 +0100

    Generated page component

---
+++ frontend/src/app/components/comunicar-indisp/comunicar-indisp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comunicar-indisp',
+  templateUrl: './comunicar-indisp.component.html',
+  styleUrls: ['./comunicar-indisp.component.css']
+})
+export class ComunicarIndispComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
