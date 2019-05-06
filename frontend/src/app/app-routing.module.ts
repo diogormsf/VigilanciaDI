25367c8f3bd4279c9ceff51178a131e4e86e43d8
Guilherme Rosa <fc47902@alunos.fc.ul.pt>
2019-05-06 03:33:50 +0100

    fixes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './components/home/home.component';
 import { AssignedFinalsComponent } from './components/assigned-finals/assigned-finals.component';
 import { ComunicarIndispComponent } from './components/comunicar-indisp/comunicar-indisp.component';
 import { LoginComponent } from './components/login/login.component';
+import { CreateCalendarComponent } from './components/create-calendar/create-calendar.component';
 import { CommonModule } from '@angular/common';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'create-calendar', component: CreateCalendarComponent },
   { path: 'assigned-finals', component: AssignedFinalsComponent },
   { path: 'comunicar-indisp', component: ComunicarIndispComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
